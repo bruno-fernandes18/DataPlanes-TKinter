c7084032bb7f193bb246cbde7fd52c6bb5dac4c4
bruno-fernandes18 <148147246+bruno-fernandes18@users.noreply.github.com>
2025-06-22 15:08:36 -0300

    Use context managers and parameterized queries

--- RAD/modules/models/planedata.py
+++ RAD/modules/models/planedata.py
@@ -1,5 +1,4 @@
 import sqlite3
-from .conn import connect_and_cursor
 
 plane_objects = {
     'approach': ('ias INTEGER', 'mcs INTEGER', 'rod INTEGER'),
@@ -32,86 +31,79 @@ plane_reference = {
 plane_order = ('approach', 'climb150', 'climb240', 'climbmach', 'cruise', 'descent100', 'initialclimb', 'initialdescent', 'landing', 'takeoff', 'technical')
 
 def boot_parts():
-    conn,cursor = connect_and_cursor('planes.db')
-    for key in plane_objects:
-        try:
-            attributes = ', '.join(plane_objects[key])
-            command = f'CREATE TABLE IF NOT EXISTS {key} (id INTEGER PRIMARY KEY, {attributes})'
-            cursor.execute(command)
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f'Error {e} when booting {key} database')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        for key in plane_objects:
+            try:
+                attributes = ', '.join(plane_objects[key])
+                command = f'CREATE TABLE IF NOT EXISTS {key} (id INTEGER PRIMARY KEY, {attributes})'
+                cursor.execute(command)
+                conn.commit()
+            except sqlite3.Error as e:
+                print(f'Error {e} when booting {key} database')
 
 def insert_part(table: str, data_to_insert: tuple):
-    conn, cursor = connect_and_cursor('planes.db')
-    if table in plane_objects:
-        try:
-            attributes = ', '.join(['?']*len(data_to_insert))
-            command = f'INSERT INTO {table} VALUES (NULL, {attributes})'
-            cursor.execute(command, data_to_insert)
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f'Error {e} when inserting data in {table} database')
-    else:
-        print("There isn't such table...")
-    part_id = cursor.lastrowid
-    
-    cursor.close()
-    conn.close()
-    
-    return part_id
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        if table in plane_objects:
+            try:
+                attributes = ', '.join(['?'] * len(data_to_insert))
+                command = f'INSERT INTO {table} VALUES (NULL, {attributes})'
+                cursor.execute(command, data_to_insert)
+                conn.commit()
+            except sqlite3.Error as e:
+                print(f'Error {e} when inserting data in {table} database')
+        else:
+            print("There isn't such table...")
+        part_id = cursor.lastrowid
+
+        return part_id
 
 def get_parts(id: int):
-    conn, cursor = connect_and_cursor('planes.db')
-    parts = {}
-    try:
-        for key in plane_objects:
-            command = f'SELECT * FROM {key} WHERE id=?'
-            cursor.execute(command,(id,))
-            information = cursor.fetchall()
-            parts[key] = information[0]
-    except sqlite3.Error as e:
-        print(f'Error {e} when getting information from Airplane parts.')
-    cursor.close()
-    conn.close()
-    return parts
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        parts = {}
+        try:
+            for key in plane_objects:
+                command = f'SELECT * FROM {key} WHERE id=?'
+                cursor.execute(command, (id,))
+                information = cursor.fetchall()
+                parts[key] = information[0]
+        except sqlite3.Error as e:
+            print(f'Error {e} when getting information from Airplane parts.')
+        return parts
 
 def get_specific_part(table: str, id: int):
-    conn, cursor = connect_and_cursor('planes.db')
-    try:
-        command = f'SELECT * FROM {table} WHERE id=?'
-        cursor.execute(command,(str(id),))
-        infolist = cursor.fetchall()
-    except sqlite3.Error as e:
-        print(f'Error {e} when getting information from Airplane parts.')
-    cursor.close()
-    conn.close()
-    return infolist
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        try:
+            command = f'SELECT * FROM {table} WHERE id=?'
+            cursor.execute(command, (id,))
+            infolist = cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f'Error {e} when getting information from Airplane parts.')
+        return infolist
 
 def update_parts(table: str, id: int, data_to_update: tuple):
-    conn, cursor = connect_and_cursor('planes.db')
-    try:
-        attributes = ', '.join([f"{piece} = ?" for piece in plane_reference[table]])
-        command = f'UPDATE {table} SET {attributes} WHERE id={id}'
-        cursor.execute(command, data_to_update)
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when updating data in {table} database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        try:
+            attributes = ', '.join([f"{piece} = ?" for piece in plane_reference[table]])
+            command = f'UPDATE {table} SET {attributes} WHERE id=?'
+            cursor.execute(command, (*data_to_update, id))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when updating data in {table} database.')
 
 def delete_parts(id_tuple: tuple):
-    conn, cursor = connect_and_cursor('planes.db')
-    try:
-        for i in range(len(plane_objects)):
-            command = f'DELETE FROM {plane_order[i]} WHERE id=?'
-            cursor.execute(command,(id_tuple[i],))
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when deleting parts from tables')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('planes.db') as conn:
+        cursor = conn.cursor()
+        try:
+            for i in range(len(plane_objects)):
+                command = f'DELETE FROM {plane_order[i]} WHERE id=?'
+                cursor.execute(command, (id_tuple[i],))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when deleting parts from tables')
 
 __all__ = ['boot_parts', 'insert_part', 'get_parts', 'get_specific_part', 'update_parts', 'delete_parts']
--- RAD/modules/models/users.py
+++ RAD/modules/models/users.py
@@ -1,64 +1,62 @@
 import sqlite3
-from .conn import connect_and_cursor
 
 def boot_user():
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, password TEXT NOT NULL)')
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when booting user database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, password TEXT NOT NULL)'
+            )
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when booting user database.')
 
 def add_user(name: str, password: str):
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('INSERT INTO users VALUES (NULL, ?, ?)', (name, password))
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when adding user to database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('INSERT INTO users VALUES (NULL, ?, ?)', (name, password))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when adding user to database.')
 
 def view_users():
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('SELECT * FROM users')
-    except sqlite3.Error as e:
-        print(f'Error {e} when selecting user from database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM users')
+        except sqlite3.Error as e:
+            print(f'Error {e} when selecting user from database.')
 
 def search_user(name: str):
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('SELECT * FROM users WHERE name=?',(name,))
-        user = cursor.fetchall()
-    except sqlite3.Error as e:
-        print(f'Error {e} when searching user from database.')
-    cursor.close()
-    conn.close()
-    return user
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM users WHERE name=?', (name,))
+            user = cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f'Error {e} when searching user from database.')
+        return user
 
 def update_user(id: int, name: str, password: str):
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('UPDATE users SET name=?, password=? WHERE id=?', (name,password,id))
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when updating user to database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(
+                'UPDATE users SET name=?, password=? WHERE id=?',
+                (name, password, id),
+            )
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when updating user to database.')
 
 def delete_user(id: int):
-    conn, cursor = connect_and_cursor('users.db')
-    try:
-        cursor.execute('DELETE FROM users WHERE id=?', (id,))
-        conn.commit()
-    except sqlite3.Error as e:
-        print(f'Error {e} when deleting user from database.')
-    cursor.close()
-    conn.close()
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('DELETE FROM users WHERE id=?', (id,))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Error {e} when deleting user from database.')
 
 __all__ = ['boot_user', 'add_user', 'view_users', 'search_user', 'update_user', 'delete_user']
